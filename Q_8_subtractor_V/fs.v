a0d75b5c195069a1569c8738234f36cdb5b5fc6e
Rakesh Gogula <gogularakesh3143@gmail.com>
2026-02-26 18:33:39 +0530

    Delete Q_8_subtractor_V directory

--- Q_8_subtractor_V/fs.v
+++
@@ -1,7 +0,0 @@
-module fs(a,b,c,diff,bo);
-	input a,b,c;
-	output diff,bo;
-
-	assign diff = a^b^c;
-	assign bo = ~a&b | b&c | ~a&c;
-endmodule
--- Q_8_subtractor_V/hs.v
+++
@@ -1,7 +0,0 @@
-module hs(a,b,diff,bo);
-	input a,b;
-	output diff,bo;
-
-	assign diff=a^b;
-	assign bo= ~a&b;
-endmodule
